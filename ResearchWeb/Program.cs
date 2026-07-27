f7ac3a59057db8b1e8a1b30ef70b613d01f830b4
My PC2 <My PC2@ADM-HR-GF-04>
2026-07-28 02:50:08 +0300

    Apply database migrations automatically on startup

--- ResearchWeb/Program.cs
+++ ResearchWeb/Program.cs
@@ -116,7 +116,12 @@ builder.Services.AddDataProtection()
 // =====================================
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
+    db.Database.Migrate();
+}
 // =====================================
 // Database Check
 // =====================================
